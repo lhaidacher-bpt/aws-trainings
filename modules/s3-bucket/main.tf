cf83a340644b8492e12cdb7499eaba3fd5d5b7ca
Lukas Haidacher <lukas.haidacher@beraingpoint.com>
2025-10-21 22:41:07 +0200

    starter implementation

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -73,6 +73,5 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
 }
 
 resource "aws_s3_bucket_notification" "staging_eventbridge" {
-  bucket      = aws_s3_bucket.this.bucket
-  eventbridge = var.enable_eventbridge_notification
+  //TODO var.enable_eventbridge_notification
 }
